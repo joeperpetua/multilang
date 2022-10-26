260a79b70942e336dfce9dca86ca75a62853cafe
joeperpetua <qwerty.perpetua@gmail.com>
2022-10-26 21:40:14 +0200

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+class Footer extends React.Component{
+
+    render(){
+        return <div>
+            <h1>Footer</h1>
+        </div>;
+    };
+
+}
+
+export default Footer;
