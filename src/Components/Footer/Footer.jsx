e4ed994e86ad2e05946b3e8f6cbbc90be9b354d8
joeperpetua <qwerty.perpetua@gmail.com>
2022-11-01 15:19:04 +0100

    adding footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,9 @@ import './Footer.css';
 class Footer extends React.Component{
 
     render(){
-        return <div>
-            <h3>Footer</h3>
+        return <div className="Footer">
+            <p>For a native mobile support download <a href="https://github.com/joeperpetua/multilang-app" rel="noreferrer" target="_blank">MultiLang Mobile App</a></p>
+            <a href="https://github.com/joeperpetua/multilang" rel="noreferrer" target="_blank"><img alt="Github Link" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/github.png"/></a>
         </div>;
     };
 
